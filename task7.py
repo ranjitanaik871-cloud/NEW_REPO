224e08d27c6e3bf663c966f0c94cf850650ef511
ranjitanaik871 <ranjitanaik871@gmail.com>
2026-07-03 16:18:45 +0530

    Badge number

---
+++ task7.py
@@ -0,0 +1,5 @@
+department_code=input("Enter the department code: ")
+joining_year=input("Enter the joining year: ")
+emp_id=input("Enter the employee ID: ")
+Employee_ID=department_code+joining_year+emp_id
+print("Employee ID:",Employee_ID)
