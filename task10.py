c8e020f19b7ea518277adf6dba532a34542eeb6f
ranjitanaik871 <ranjitanaik871@gmail.com>
2026-07-03 16:43:13 +0530

    employee egilibity

---
+++ task10.py
@@ -0,0 +1,7 @@
+emp_age=int(input("Enter the employee age: "))
+if emp_age>=58:
+    print("Eligible for retirement.")
+elif emp_age>=18:
+    print("enroll for fund")
+else:
+    print("Not eligible for retirement or fund enrollment.")
